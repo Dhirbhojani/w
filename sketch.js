c5122d493e44aab395508ab7a418f260837e09c6
Dhirbhojani <72383305+Dhirbhojani@users.noreply.github.com>
2021-06-10 13:12:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("black");
+  hr = hour();mn = minute();sc = second();
+
+  push();
+  translate(200,200)
+  scAngle = (sc,0,60,0,360);
+  stroke(225,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  
+  pop()
+  
+    
+  drawSprites();
+  
+}
